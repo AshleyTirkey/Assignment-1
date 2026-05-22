f958e0a99fb09d2a8842c8d1a2fae9e97134ca44
Ashley Tirkey <87265518+AshleyTirkey@users.noreply.github.com>
2026-05-22 12:07:47 +0200

    Update generator.ts

--- src/generator.ts
+++ src/generator.ts
@@ -2,60 +2,63 @@ import { javascriptGenerator } from 'blockly/javascript';
 
 export const generator = javascriptGenerator;
 
-generator.forBlock['rbac_role'] = function (block) {
-  const role = block.getFieldValue('ROLE');
+generator.forBlock["rbac_role"] = function(block) {
+  const role = block.getFieldValue("ROLE");
+
   return `"${role}"\n`;
 };
 
-generator.forBlock['rbac_resource'] = function (block) {
-  const res = block.getFieldValue('RES');
-  return `"${res}"\n`;
+generator.forBlock["rbac_resource"] = function(block) {
+  const resource = block.getFieldValue("RES");
+
+  return `"${resource}"\n`;
 };
 
-generator.forBlock['rbac_rule'] = function (block) {
+generator.forBlock["rbac_rule"] = function(block, generator) {
+  const effect = block.getFieldValue("EFFECT");
+  const action = block.getFieldValue("ACTION");
 
-  const effect = block.getFieldValue('EFFECT');
-  const action = block.getFieldValue('ACTION');
+  const resourcesCode = generator.statementToCode(block, "RESOURCES");
 
-  const returnVal =
-    effect === 'ALLOW' ? 'Allow' : 'Deny';
+  const resources = resourcesCode
+    .split("\n")
+    .map(line => line.trim())
+    .filter(line => line !== "");
 
-  let resources: any =
-    generator.statementToCode(block, 'RESOURCES');
-
-  resources = resources
-    .split(',')
-    .filter((r: string) => r.trim() !== '');
+  const result = effect === "ALLOW"
+    ? "Allow"
+    : "Deny";
 
   return `
-if (action === "${action}" && [${resources}].includes(resource)) {
-  return "${returnVal}";
+if (
+  action === "${action}" &&
+  [${resources.join(", ")}].includes(resource)
+) {
+  return "${result}";
 }
 `;
 };
 
-generator.forBlock['rbac_policy'] = function (block) {
+generator.forBlock["rbac_policy"] = function(block, generator) {
+  const name = block.getFieldValue("NAME");
 
-  let roles: any =
-    generator.statementToCode(block, 'ROLES');
+  const rolesCode = generator.statementToCode(block, "ROLES");
 
-  roles = roles
-    .split(',')
-    .filter((r: string) => r.trim() !== '');
+  const roles = rolesCode
+    .split("\n")
+    .map(line => line.trim())
+    .filter(line => line !== "");
 
-  const rules =
-    generator.statementToCode(block, 'RULES');
-
-  const name = block.getFieldValue('NAME');
+  const rulesCode = generator.statementToCode(block, "RULES");
 
   return `
 function ${name}(role, action, resource) {
 
-  if (![${roles}].includes(role)) {
+  if (![${roles.join(", ")}].includes(role)) {
     return "Deny";
   }
 
-  ${rules}
+  ${rulesCode}
 
   return "Deny";
 }
