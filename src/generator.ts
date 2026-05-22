e9466e8611cacdaa0200d077c6946982e43efee5
Ashley Tirkey <87265518+AshleyTirkey@users.noreply.github.com>
2026-05-22 11:26:35 +0200

    Update generator.ts

--- src/generator.ts
+++ src/generator.ts
@@ -1,16 +1,63 @@
-import * as Blockly from 'blockly';
+import { javascriptGenerator } from 'blockly/javascript';
 
-import { javascriptGenerator} from 'blockly/javascript';
 export const generator = javascriptGenerator;
 
-generator.INDENT = '  ';
-
-// TODO add your code generation logic here.
-// https://developers.google.com/blockly/guides/create-custom-blocks/generating-code
-
-generator.forBlock['rbac_policy'] = function (block : Blockly.Block) : string {
-  // TODO
-  return 'Deny'; // placeholder
+generator.forBlock['rbac_role'] = function (block) {
+  const role = block.getFieldValue('ROLE');
+  return `"${role}",`;
 };
 
-// TODO and also implement the other ones...
+generator.forBlock['rbac_resource'] = function (block) {
+  const res = block.getFieldValue('RES');
+  return `"${res}",`;
+};
+
+generator.forBlock['rbac_rule'] = function (block) {
+
+  const effect = block.getFieldValue('EFFECT');
+  const action = block.getFieldValue('ACTION');
+
+  const returnVal =
+    effect === 'ALLOW' ? 'Allow' : 'Deny';
+
+  let resources: any =
+    generator.statementToCode(block, 'RESOURCES');
+
+  resources = resources
+    .split(',')
+    .filter((r: string) => r.trim() !== '');
+
+  return `
+if (action === "${action}" && [${resources}].includes(resource)) {
+  return "${returnVal}";
+}
+`;
+};
+
+generator.forBlock['rbac_policy'] = function (block) {
+
+  let roles: any =
+    generator.statementToCode(block, 'ROLES');
+
+  roles = roles
+    .split(',')
+    .filter((r: string) => r.trim() !== '');
+
+  const rules =
+    generator.statementToCode(block, 'RULES');
+
+  const name = block.getFieldValue('NAME');
+
+  return `
+function ${name}(role, action, resource) {
+
+  if (![${roles}].includes(role)) {
+    return "Deny";
+  }
+
+  ${rules}
+
+  return "Deny";
+}
+`;
+};
