15cf0d65ffb517cb8c028c560a492de9697c7648
Ashley Tirkey <87265518+AshleyTirkey@users.noreply.github.com>
2026-05-22 11:27:25 +0200

    Update blocks.ts

--- src/blocks.ts
+++ src/blocks.ts
@@ -1,25 +1,100 @@
 import * as Blockly from 'blockly';
 
-// Export a function to define our blocks
 export function defineBlocks() {
   Blockly.defineBlocksWithJsonArray([
-    // TODO add your block definitions here.
-    // https://developers.google.com/blockly/guides/create-custom-blocks/define/block-definitions
+
     {
       "type": "rbac_policy",
-      // TODO
+      "message0": "Policy Name %1 Roles %2 Rules %3",
+      "args0": [
+        {
+          "type": "field_input",
+          "name": "NAME",
+          "text": "Policy"
+        },
+        {
+          "type": "input_statement",
+          "name": "ROLES",
+          "check": "rbac_role"
+        },
+        {
+          "type": "input_statement",
+          "name": "RULES",
+          "check": "rbac_rule"
+        }
+      ],
+      "colour": 230
     },
+
     {
       "type": "rbac_role",
-      // TODO      
+      "message0": "Role %1",
+      "args0": [
+        {
+          "type": "field_dropdown",
+          "name": "ROLE",
+          "options": [
+           ["TrustandSafetyLead", "TRUSTANDSAFETYLEAD"],
+           ["Moderator", "MODERATOR"],
+           ["User", "USER"]
+          ]
+        }
+      ],
+      "previousStatement": "rbac_role",
+      "nextStatement": "rbac_role",
+      "colour": 120
     },
+
     {
       "type": "rbac_rule",
-      // TODO      
+      "message0": "Effect %1 Action %2 Resources %3",
+      "args0": [
+        {
+          "type": "field_dropdown",
+          "name": "EFFECT",
+          "options": [
+            ["Allow", "ALLOW"],
+            ["Deny", "DENY"]
+          ]
+        },
+        {
+          "type": "field_dropdown",
+          "name": "ACTION",
+          "options": [
+            ["Ban", "BAN"],
+            ["Flag", "FLAG"],
+            ["Post", "POST"]
+          ]
+        },
+        {
+          "type": "input_statement",
+          "name": "RESOURCES",
+          "check": "rbac_resource"
+        }
+      ],
+      "previousStatement": "rbac_rule",
+      "nextStatement": "rbac_rule",
+      "colour": 20
     },
+
     {
       "type": "rbac_resource",
-      // TODO
+      "message0": "Resource %1",
+      "args0": [
+        {
+          "type": "field_dropdown",
+          "name": "RES",
+          "options": [
+            ["UserAccounts", "USERACCOUNTS"],
+            ["ReportedContent", "REPORTEDCONTENT"],
+            ["PublicTimeline", "PUBLICTIMELINE"]
+          ]
+        }
+      ],
+      "previousStatement": "rbac_resource",
+      "nextStatement": "rbac_resource",
+      "colour": 65
     }
+
   ]);
 }
